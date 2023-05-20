ca2a58e7233c3d4dd696f3d43c6b5224f434bbe6
Francis Osei <oseifrancis673@gmail.com>
2023-05-20 22:46:14 +0100

    modify config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,25 +2,25 @@ require("dotenv").config();
 
 
 module.exports = {
-  "development": {
-    "username": process.env.USER_NAME,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE,
-    "host": "127.0.0.1",
-    "dialect": "postgres",
+  development: {
+    username: process.env.USER_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "production": {
-    "username": process.env.USER_NAME,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE,
-    "host": "127.0.0.1",
-    "dialect": "postgres",
+  production: {
+    username: process.env.USER_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
   },
 };
