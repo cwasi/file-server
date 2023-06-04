9e8d9187ca63bf0bdf9f0730406eb468e75560f6
Francis Osei <oseifrancis673@gmail.com>
2023-06-04 08:27:34 +0100

    modify app

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.enable('trust proxy');
 
 // Set up template engine PUG
 app.set('view engine', 'pug');
-app.set('views', path.join('views'));
+app.set('views', path.join(__dirname,'views'));
 
 app.use(cors())
 app.options('*', cors());
@@ -35,7 +35,7 @@ app.use(express.static(path.join('public')));
 
 // Body passer, reading data from body
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 app.use(cookieParser());
 
 // Development logging
